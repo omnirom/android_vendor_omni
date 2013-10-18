c7bc0c789ab06ababbd9edc0221c0582a81715f2
UtkarshGupta <utkarsh.eminem@gmail.com>
2013-10-18 10:54:34 +0530

    Roomservice
    Thanks to PAC-man Team. Copy pasted most of the the stuff.
    Special thanks to Djoko Santoko (aka szl.kiev) & ExPeacer for the python scripts
    
    Usage:
    Check the syntax in dependencies file.
    You can make other changes in .mk
    
    Change-Id: I1548e2ee3b216594c14852a623d22054bf3ca3d9

--- config/omni_common.mk
+++ config/omni_common.mk
@@ -44,11 +44,77 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Additional packages
--include vendor/omni/config/packages.mk
-
-# Versionning
--include vendor/omni/config/version.mk
-
 # Add our overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
+
+# Additional packages
+PRODUCT_PACKAGES += \
+	Development \
+	LatinIME \
+	VideoEditor \
+	VoiceDialer \
+	SoundRecorder \
+	Basic
+
+# Additional apps
+PRODUCT_PACKAGES += \
+	Apollo \
+	DashClock \
+	DSPManager \
+	libcyanogen-dsp \
+	audio_effects.conf \
+	MonthCalendarWidget
+
+PRODUCT_PACKAGES += \
+	CellBroadcastReceiver
+
+# Additional tools
+PRODUCT_PACKAGES += \
+	openvpn \
+	e2fsck \
+	mke2fs \
+	tune2fs \
+	bash \
+	vim \
+	nano \
+	htop \
+	powertop \
+	lsof
+
+# Versioning of the ROM
+PRODUCT_VERSION_MAJOR = 4
+PRODUCT_VERSION_MINOR = 3
+PRODUCT_VERSION_REVISION = 0
+
+ifdef BUILDTYPE_NIGHTLY
+	ROM_BUILDTYPE := NIGHTLY
+endif
+ifdef BUILDTYPE_AUTOTEST
+	ROM_BUILDTYPE := AUTOTEST
+endif
+ifdef BUILDTYPE_EXPERIMENTAL
+	ROM_BUILDTYPE := EXPERIMENTAL
+endif
+ifdef BUILDTYPE_RELEASE
+	ROM_BUILDTYPE := RELEASE
+endif
+
+ifndef ROM_BUILDTYPE
+	ROM_BUILDTYPE := HOMEMADE
+endif
+
+# Build the final version string
+ifdef BUILDTYPE_RELEASE
+	ROM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(PRODUCT_VERSION_REVISION)-$(TARGET_PRODUCT)
+else
+	ROM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(PRODUCT_VERSION_REVISION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT)-$(ROM_BUILDTYPE)
+endif
+
+# Apply it to build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.modversion=$(ROM_VERSION)
+
+# Disable ADB authentication and set root access to Apps and ADB
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    persist.sys.root_access=3
