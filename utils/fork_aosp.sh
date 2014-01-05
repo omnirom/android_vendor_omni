0cb714aef87ae11fc70d8d7d4731c60d8b4c28a0
XpLoDWilD <me@xplod.fr>
2014-01-05 15:37:48 +0100

    utils: Update AOSP forking script
    Change-Id: Ifbb68a5940c079b8b0898e6a7d66df92d9077c28

--- utils/fork_aosp.sh
+++ utils/fork_aosp.sh
@@ -3,21 +3,20 @@
 # Licensed under GPLv3
 
 # Configuration
-CONFIG_ROOT=~/gitolite-admin/
-CONFIG_FILE=$CONFIG_ROOT/conf/gitolite.conf
-ANDROID_ROOT=~/android
+ANDROID_ROOT=~/omni
 PREFIX=android_
-BRANCH=android-4.3
-SOURCE=android-4.3_r2.2
+BRANCH=android-4.4
+SOURCE=android-4.4.2_r1
 MANIFEST=android/default.xml
-XDA_REMOTE=ssh://git@localhost # NO TRAILING SLASH
-GERRIT_REMOTE=ssh://build1.dev.xda-developers.com:29418
-REMOTE_MANIFEST=private
+GITHUB_ORG=omnirom
+USERNAME=xplodwild
+GERRIT_REMOTE=ssh://gerrit.omnirom.org:29418
+REMOTE_MANIFEST=omnirom
 
 # Script
 if [ $# -lt 1 ]; then
-        echo Usage: ./clone.sh path
-        echo Example: ./clone.sh frameworks/base
+        echo Usage: ./fork_aosp.sh path
+        echo Example: ./fork_aosp.sh frameworks/base
         exit 1
 fi
 
@@ -32,40 +31,23 @@ if [ ! -d .git ]; then
 fi
 popd
 
-CURRENT_DIR=$(pwd)
-cd $CONFIG_ROOT
-git pull
-cd $CURRENT_DIR
-
-# Create the repo inside gitolite
-echo "Creating $REPO_NAME..."
-
-echo "" >> $CONFIG_FILE
-echo "repo $REPO_NAME" >> $CONFIG_FILE
-echo "    RW+     =  pulser" >> $CONFIG_FILE
-echo "    RW+     =  xplodwild" >> $CONFIG_FILE
-echo "    R       =  @all" >> $CONFIG_FILE
-echo "" >> $CONFIG_FILE
-
-echo "Comitting and pushing..."
-CURRENT_DIR=$(pwd)
-cd $CONFIG_ROOT
-git commit conf/gitolite.conf -m "Add $REPO_NAME"
-git push
+# Create the repo at github
+echo "Creating $REPO_NAME on GitHub..."
+curl --user $USERNAME --data "{\"name\":\"$REPO_NAME\"}" https://api.github.com/orgs/$GITHUB_ORG/repos
 
 # Only works if you are a gerrit admin, will create the named project before pushing (gerrit then replicates to git)
-
 ssh -p 29418 gerrit.omnirom.org gerrit create-project --name $REPO_NAME
 
 echo "Creating branch $BRANCH..."
-cd $CURRENT_DIR/$1
+pushd $1
+git remote rm gerrit
 git remote add gerrit $GERRIT_REMOTE/$REPO_NAME
 git checkout $SOURCE
 git branch $BRANCH
 git push gerrit $BRANCH
 
 echo "Updating manifest..."
-cd $ANDROID_ROOT
+popd
 SRC_LINE=$(cat $MANIFEST | grep ${1%/} | head -n1)
 if [[ "$SRC_LINE" == *group="pdk"* ]]; then
         NEW_LINE="  <project path=\"${1%/}\" name=\"$REPO_NAME\" remote=\"$REMOTE_MANIFEST\" revision=\"$BRANCH\" group=\"pdk\" />"
@@ -78,10 +60,11 @@ fi
 sed -i "s%$SRC_LINE%$NEW_LINE%g" $MANIFEST
 
 echo "Pushing manifest"
-cd $ANDROID_ROOT/android
+pushd android
 git checkout $BRANCH
 git commit -a -m "Replace $1 path to new repository $REPO_NAME"
 git push $GERRIT_REMOTE/android HEAD:refs/for/$BRANCH
+popd
 
 echo "Now remember to approve the change on gerrit before going further!"
 read -p "Press [Enter] key once you've approved the change on gerrit"
