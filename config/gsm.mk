d599115ef24419331d932ee7cc18a5967587aa12
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-01-11 18:50:54 -0600

    Update gsm.mk

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,6 +1,6 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/Anime/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
