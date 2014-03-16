4aa71e2a015772f67256f108649fb466a7a4dfda
Andreas B <skate4life@gmx.de>
2014-03-16 13:40:20 +0100

    Vendor: Add common-tablet.mk and common-tablet overlay
    Change-Id: Iba072a50ce2167694c43a53175287c9e153477ec

---
+++ config/common_tablet.mk
@@ -0,0 +1,8 @@
+# Common settings and files
+-include vendor/omni/config/common.mk
+
+# Add tablet overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common_tablet
+
+PRODUCT_CHARACTERISTICS := tablet
+
