82b92ca04fe6175ab855a1e8df4ef32f9f2bda88
Max Weninger <max.weninger@gmail.com>
2014-03-22 19:28:54 +0100

    Revert "Omni: Add LowerFPS Bootanimantion"
    This reverts commit 3df3e313674617a176061b64df8560807d3bd5ea.
    
    Change-Id: Ia9bc9936bd93b7a55504a3e39e156a4b3a7a84d5

--- config/common.mk
+++ config/common.mk
@@ -1,13 +1,8 @@
 PRODUCT_BRAND ?= omni
 
-# bootanimation (Some devices cant go over 100fps for a bootani)
-ifeq ($(BOARD_NEEDS_LOWFPS_BOOTANI),)
-PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/lowfps-bootanimation.zip:system/media/bootanimation.zip
-else
+# bootanimation
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
-endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
