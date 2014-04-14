9725047bbca2ca43705dc22568f645f635215984
Shareef Ali <shareefalis@cyanogenmod.org>
2014-04-14 12:26:39 +0530

    omni: World phone support
    Change-Id: Ia231f23f9c2f4e3ef193954b4aab45f6125cd51f

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,6 +1,10 @@
-# GSM APN list
+# World APN list
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
