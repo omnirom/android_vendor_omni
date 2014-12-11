07caab5682c0f5de365de4b8e13e9fe3abccb05d
maxwen <max.weninger@gmail.com>
2014-12-11 01:39:44 +0100

    sepolicy: allow access for perfomance settings
    Change-Id: Id8b66ae5bd92ba301b3d652501aa27b0eadb36ea

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -6,10 +6,13 @@
 BOARD_SEPOLICY_DIRS += \
     vendor/omni/sepolicy
 
-#BOARD_SEPOLICY_UNION += \
-#    file_contexts \
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    system_app.te \
+    system_server.te
+
 #    file.te \
 #    genfs_contexts \
 #    installd.te \
 #    mac_permissions.xml \
-#	vold.te
+#    vold.te
