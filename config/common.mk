a5dfd9fffeb06461e2e4a6fa4e3b94a0066bb2dd
Humberto Borba <humberos@gmail.com>
2015-01-31 16:50:25 -0200

    vendor: omni: use specific resolution for bootanimation
    the original idea came from this patch
    https://github.com/CyanogenMod/android_vendor_cm/commit/367e64830fde67dc689d56e733c85dfbb30b8ae1
    
    PS2:
    -added hdmi and qhd
    
    PS3:
    -use correct geometry to match landscape
    
    PS4:
    -use resolution as geometry
    
    PS5|6:
    -fix code style
    
    PS7:
    -add 720x480
         840x560
         960x640
    
    PS8:
    -add 2560x1440
    
    PS9:
    -use TARGET_BOOTANIMATION_SIZE instead of TARGET_BOOTANIMATION_NAME
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I36ca35dbdb638cc29f29cf2a2c619c3cbd3c1034

--- config/common.mk
+++ config/common.mk
@@ -1,12 +1,12 @@
 PRODUCT_BRAND ?= omni
 
-# bootanimation (Some devices cant go over 100fps for a bootani)
-ifneq ($(USE_LOWFPS_BOOTANI),true)
+# use specific resolution for bootanimation
+ifneq ($(TARGET_BOOTANIMATION_SIZE),)
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/omni/prebuilt/bootanimation/res/$(TARGET_BOOTANIMATION_SIZE).zip:system/media/bootanimation.zip
 else
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bootanimation/lowfps-bootanimation.zip:system/media/bootanimation.zip
+    vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
