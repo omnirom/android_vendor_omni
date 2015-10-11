03adf8bf4c1c5a3fae4db51cdc66bd5846548b0f
Jake Whatley <jacob.whatley91@gmail.com>
2015-10-10 23:37:01 -0400

    sepolicy: remove BOARD_SEPOLICY_UNION
    per upstream discussion here: https://android-review.googlesource.com/#/c/141560/
    
    Change-Id: I4ea1561df70e8a3ad5b012f8540a57fd3e55519f

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,21 +5,3 @@
 
 BOARD_SEPOLICY_DIRS += \
     vendor/omni/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    adbd.te \
-    file_contexts \
-    file.te \
-    genfs_contexts \
-    healthd.te \
-    property_contexts \
-    installd.te \
-    property.te \
-    shell.te \
-    sysinit.te \
-    system.te \
-    system_app.te \
-    system_server.te \
-    ueventd.te \
-    userinit.te \
-    vold.te
