3453f88de4a34f8230cbac83ede33e7cc5e155eb
Hernan Castanon Alvarez <hernanderes34@gmail.com>
2015-11-02 07:38:38 -0800

    omni: add TARGET_USES_SNAPCAMERA
    Change-Id: Ibac2768927b64429f636c8c9e0e12e93ddda0d8c

--- config/packages.mk
+++ config/packages.mk
@@ -15,6 +15,11 @@ PRODUCT_PACKAGES += \
     OpenDelta \
     OmniSwitch
 
+ifeq ($(TARGET_USES_SNAPCAMERA), true)
+PRODUCT_PACKAGES += \
+    SnapdragonCamera
+endif
+
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver
 
