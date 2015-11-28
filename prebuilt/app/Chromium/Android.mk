15641fd954c1e518dc39250180edc077d191180b
Menno van Grinsven <scanno71@gmail.com>
2015-11-28 22:26:01 +0100

    [2/2] vendor: use prebuilt chromium
    Devices can continue to use AOSP browser by using
    TARGET_USES_AOSP_BROWSER := true
    else prebuilt chromium is used
    
    Change-Id: I9129cbb91f98d7a72f8a2bdf470f81ff7d9ce481

---
+++ prebuilt/app/Chromium/Android.mk
@@ -0,0 +1,23 @@
+ifneq ($(TARGET_USES_AOSP_BROWSER),true)
+LOCAL_PATH:= $(call my-dir)
+
+ifeq ($(TARGET_ARCH),arm64)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := Chromium
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES    := 64bit/ChromePublic.apk
+LOCAL_CERTIFICATE  := PRESIGNED
+LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
+include $(BUILD_PREBUILT)
+else
+include $(CLEAR_VARS)
+LOCAL_MODULE       := Chromium
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES    := 32bit/ChromePublic.apk
+LOCAL_CERTIFICATE  := PRESIGNED
+LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
+include $(BUILD_PREBUILT)
+endif
+endif
