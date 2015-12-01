1f09894e3322fc02c9e073a60dbc5828fe9dd7a2
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-01 21:04:28 +0100

    spn: add selective-spn-conf and LG U+
    Change-Id: Ibbb5e68e026ae424f2fe71379ca25c645d1e918a

--- config/gsm.mk
+++ config/gsm.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
