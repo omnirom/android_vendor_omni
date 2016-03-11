4e1248a0979d732a8f57000f27360ad6307d1b4e
Jake Whatley <jacob.whatley91@gmail.com>
2016-03-10 21:22:56 -0500

    vendor: add aosp merge script
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Signed-off-by: Jake Whatley <jacob.whatley91@gmail.com>
    Change-Id: I2fb57d51858a562dc701d0f1e32b9c6e94ef03dd

---
+++ utils/aosp-merge.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+echo -e "Enter the AOSP ref to merge"
+read ref
+
+cd ../../../
+
+while read path;
+    do
+
+    project=`echo android_${path} | sed -e 's/\//\_/g'`
+
+    echo ""
+    echo "====================================================================="
+    echo " PROJECT: ${project} -> [ ${path}/ ]"
+    echo ""
+
+    cd $path;
+
+    git merge --abort;
+
+    repo sync -d .
+
+    if git branch | grep "android-6.0-merge" > /dev/null; then
+        git branch -D android-6.0-merge > /dev/null
+    fi
+
+    repo start android-6.0-merge .
+
+    if ! git remote | grep "aosp" > /dev/null; then
+        git remote add aosp https://android.googlesource.com/platform/$path > /dev/null
+    fi
+
+    git fetch aosp
+
+    #echo "====================================================================="
+    #echo " Merging {$ref}"
+    #echo "====================================================================="
+    git merge $ref;
+
+    cd - > /dev/null
+
+done < vendor/omni/utils/omni-list
---
+++ utils/omni-push.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+echo -e "Enter the username"
+read username
+
+url="gerrit.omnirom.org"
+port="29418"
+branch="android-6.0"
+
+cd ../../../
+
+while read path;
+    do
+
+    project=`echo android_${path} | sed -e 's/\//\_/g'`
+
+    echo ""
+    echo "====================================================================="
+    echo " PROJECT: ${project} -> [ ${path}/ ]"
+    echo ""
+
+    cd $path;
+
+    echo " Pushing..."
+    echo " git push --no-thin ssh://${username}@${url}:${port}/${project} HEAD:refs/heads/${branch}"
+    git push --no-thin ssh://${username}@${url}:${port}/${project} HEAD:refs/heads/${branch}
+    echo ""
+
+    cd - > /dev/null
+
+done < vendor/omni/utils/omni-list
