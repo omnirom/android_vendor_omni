5b743b09c983ea53596700e8091319bbc886be02
Jake Whatley <jacob.whatley91@gmail.com>
2016-08-27 02:14:52 +0200

    vendor: remove remnants of cdma.mk and cdma apns
    Change-Id: Icbf23b06dda3bcd18c377712d5e44d4655dce595

--- config/apns.mk
+++ config/apns.mk
@@ -1,7 +1,8 @@
-# GSM APN list
+# APN list
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# SPN list
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
