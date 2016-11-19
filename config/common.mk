3697011395182f89da0261f5b85bbf2ffdc409fc
maxwen <max.weninger@gmail.com>
2016-11-19 16:07:31 +0100

    omni: add SECURITY_RELEASE to WITH_DEXPREOPT list
    Change-Id: Ibdfc70366f3e2d1e1c74f90b1c064cdc883737bd

--- config/common.mk
+++ config/common.mk
@@ -74,3 +74,12 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
 ifeq ($(ROM_BUILDTYPE),NIGHTLY)
     WITH_DEXPREOPT := true
 endif
+# and weeklies
+ifeq ($(ROM_BUILDTYPE),WEEKLY)
+    WITH_DEXPREOPT := true
+endif
+# and security releases
+ifeq ($(ROM_BUILDTYPE),SECURITY_RELEASE)
+    WITH_DEXPREOPT := true
+endif
+
