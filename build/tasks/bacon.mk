c6a83481488360fe6f80b0b904113905182d2d7e
André Pinela <sheffzor@gmail.com>
2017-01-22 12:51:01 +0000

    tasks: enhancement to create LineageOS ota package
    - modify for OmniRom
    - add bacon target
    - don't touch aosp otapackage target
    
    Change-Id: Id243c3099b4914ed3b53efc42c0c7dac9d56dea8
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ build/tasks/bacon.mk
@@ -0,0 +1,25 @@
+# Copyright (C) 2017 Unlegacy-Android
+# Copyright (C) 2017 OmniRom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# OmniRoms OTA update package
+
+CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/omni-$(ROM_VERSION).zip
+
+.PHONY: bacon
+bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CUSTOM_TARGET_PACKAGE)
+	$(hide) $(MD5SUM) $(CUSTOM_TARGET_PACKAGE) | cut -d ' ' -f1 > $(CUSTOM_TARGET_PACKAGE).md5sum
+	@echo -e "Package complete: $(CUSTOM_TARGET_PACKAGE)"
