9c02a9bca1440d1ae4d6ccad435bee5ec59d699a
Luca Stefani <luca020400@lineageos.org>
2017-08-17 20:34:44 +0200

    vendor: build: Kill schedtool
    Change-Id: Idd6cc6679b1e84f365a5a18645590c549af20af5

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -79,14 +79,7 @@ function fixup_common_out_dir() {
 
 # Make using all available CPUs
 function mka() {
-    case `uname -s` in
-        Darwin)
-            m -j "$@"
-            ;;
-        *)
-            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
-            ;;
-    esac
+    m -j "$@"
 }
 
 function pushboot() {
