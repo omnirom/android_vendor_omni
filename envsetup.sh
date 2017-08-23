09cb15ff0f47a03d278fef34a42fd2ff4eb1c5b5
Andreas Blaesius <skate4life@gmx.de>
2017-08-24 00:23:47 +0200

    vendor: add more build commands
        build: Add bacon target, and mka/brunch/breakfast aliases
    
        Change-Id: I06d70a56c6946d7a23c6df81769aa20e7500f953
    
        brunch: custom->omni
    
        Change-Id: I5482c765a7c78d6caae371d6fabb8f55e10ac655
    
        Build: Add support for specifying build variant in brunch/breakfast
    
        This little modification allows specifying build variant in brunch/breakfast commands. For example we can use "brunch i9300 user" to build user variant instead of default userdebug.
    
        When no extra argument is given, userdebug is default.
    
        Change-Id: I2d438209f8a14949c64baa66485c811da384d20b
    
    Change-Id: I2af7116231393b40afef796a160f49926248b8f2

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,64 @@
 function repopick() {
     set_stuff_for_environment
     T=$(gettop)
-    $T/build/tools/repopick.py $@
+    $T/vendor/omni/build/tools/repopick.py $@
+}
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        time mka bacon
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local variant=$2
+    CUSTOM_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/omni/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the omni model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch omni_$target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
+# Make using all available CPUs
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
 }
