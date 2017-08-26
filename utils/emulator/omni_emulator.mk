433437308aed3a61ddfd880a8c51138a90e4fc12
maxwen <max.weninger@gmail.com>
2017-08-26 17:16:24 +0200

    omni: helpers for creating omni emulator builds
    Precondition: android SDK must be installed in ~/Android/SDK
    
    lunch omni_emulator-userdebug
    make
    
    if you want to run on the same host
    vendor/omni/utils/emulator/start_emulator_local.sh
    
    if you need to copy on a different host
    vendor/omni/utils/emulator/create_emulator_image.sh
    cp /tmp/omni_emulator.zip to the device where you have installed android SDK
    unzip into /tmp
    run /tmp/generic_x86/start_emulator_image.sh
    
    Change-Id: I1b2abeb769a21d2da21e058d9767974cad99bed7

---
+++ utils/emulator/omni_emulator.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += \
+    vendor/omni/utils/emulator/fstab.ranchu:root/fstab.ranchu
+
+$(call inherit-product, build/target/product/sdk_x86.mk)
+
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+$(call inherit-product, vendor/omni/utils/emulator/common.mk)
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_emulator
