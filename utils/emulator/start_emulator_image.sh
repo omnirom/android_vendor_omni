01620e309328c715c26ccb4a89075843d8991954
maxwen <max.weninger@gmail.com>
2017-08-26 17:16:24 +0200

    omni: helpers for creating omni emulator builds
    Precondition: android SDK must be installed in ~/Android/SDK
    
    lunch omni_emulator-userdebug
    make
    
    if you want to run on the same host
    vendor/omni/utils/emulator/start_emulator_local.sh
    
    if you need to copy on a different host
    vendor/omni/utils/emulator/create_emulator_image.sh
    cp /tmp/omni_emulator.zip to the device where you have installed android SDK
    unzip into /tmp
    run /tmp/generic_x86/start_emulator_image.sh
    
    Change-Id: I1b2abeb769a21d2da21e058d9767974cad99bed7

---
+++ utils/emulator/start_emulator_image.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# assumes the image zip has been extracted in /tmp
+export ANDROID_BUILD_TOP=/tmp/generic_x86
+export ANDROID_PRODUCT_OUT=/tmp/generic_x86
+
+~/Android/Sdk/tools/emulator  -verbose -skindir /tmp/generic_x86/skins/ -skin pixel_xl -kernel ~/Android/Sdk/system-images/android-26/google_apis/x86/kernel-ranchu -gpu host -ramdisk ~/Android/Sdk/system-images/android-26/google_apis/x86/ramdisk.img
