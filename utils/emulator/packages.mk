33f593be45b87b66f3f3dd258cf845aa6cb34b95
maxwen <max.weninger@gmail.com>
2017-08-26 17:16:24 +0200

    omni: helpers for creating omni emulator builds
    Precondition: android SDK must be installed in ~/Android/SDK
    
    lunch omni_emulator-userdebug
    make
    
    if you want to run on the same host
    vendor/omni/utils/emulator/start_emulator_local.sh
    
    if you need to copy on a different host
    vendor/omni/utils/emulator/create_emulator_image.sh
    cp /tmp/omni_emulator.zip to the device where you have installed android SDK
    unzip into /tmp
    run /tmp/generic_x86/start_emulator_image.sh
    
    Change-Id: I1b2abeb769a21d2da21e058d9767974cad99bed7

---
+++ utils/emulator/packages.mk
@@ -0,0 +1,40 @@
+# Additional apps
+PRODUCT_PACKAGES += \
+    MonthCalendarWidget \
+    OpenDelta \
+    OmniSwitch \
+    Chromium \
+    OmniJaws \
+    OmniStyle \
+    QuickSearchBox \
+    OmniClockOSS \
+    MusicFX \
+    audio_effects.conf \
+    libcyanogen-dsp \
+    Phonograph \
+    Turbo \
+    MatLog \
+    GoldfishParts
+
+# Themes
+PRODUCT_PACKAGES += \
+    PixelTheme \
+    Stock
+
+# Additional tools
+PRODUCT_PACKAGES += \
+    bash \
+    e2fsck \
+    fsck.exfat \
+    htop \
+    lsof \
+    mke2fs \
+    mount.exfat \
+    nano \
+    openvpn \
+    powertop \
+    tune2fs \
+    vim \
+    mkfs.ntfs \
+    mount.ntfs \
+    fsck.ntfs
