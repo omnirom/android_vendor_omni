f8ab4624b8c5b6090c516b5a3ca9fbc4d72429cf
maxwen <max.weninger@gmail.com>
2017-09-01 13:33:01 +0200

    omni: remove substratum packages from build
    Change-Id: I5425a69ab41c9bb8ef76d8e5ced68c6176347336

--- config/packages.mk
+++ config/packages.mk
@@ -1,10 +1,3 @@
-# Additional packages
-ifneq ($(TARGET_LOW_RAM_DEVICE), true)
-PRODUCT_PACKAGES += \
-    Basic \
-    Development
-endif
-
 # Additional apps
 PRODUCT_PACKAGES += \
     MonthCalendarWidget \
@@ -20,9 +13,7 @@ PRODUCT_PACKAGES += \
     libcyanogen-dsp \
     Phonograph \
     Turbo \
-    MatLog \
-    OmniTheme \
-    OmniSubs
+    MatLog
 
 # Additional tools
 PRODUCT_PACKAGES += \
@@ -47,7 +38,9 @@ PRODUCT_PACKAGES += telephony-ext
 PRODUCT_BOOT_JARS += telephony-ext
 
 # OMS support
-PRODUCT_PACKAGES += ThemeInterfacer
+#PRODUCT_PACKAGES += ThemeInterfacer \
+    OmniTheme \
+    OmniSubs
 
 # for easter fun
-PRODUCT_PACKAGES += EggGame
+#PRODUCT_PACKAGES += EggGame
