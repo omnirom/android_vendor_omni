0e3159170c07e7b3b840e5bdd82330de7f4d8012
Marko Man <darkobas@gmail.com>
2017-09-12 02:24:39 +0200

    vendor: move backup scripts
    Change-Id: I7550942e54e0dfbcd5282da7024489428284ceff

--- config/common.mk
+++ config/common.mk
@@ -27,8 +27,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/omni/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/omni/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/omni/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/omni/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
     vendor/omni/prebuilt/bin/blacklist:system/addon.d/blacklist
 
