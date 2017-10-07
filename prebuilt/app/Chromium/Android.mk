a2cb9c2eeed5836c011122c9cbce450d663a951a
Menno van Grinsven <scanno71@gmail.com>
2017-10-07 10:50:28 +0200

    omni: update chromiumn to 61 and use new build target
    Change-Id: I2528e9925c1ffe56325a77b6a57c09493f7a609b
    (cherry picked from commit 7c71592e5d75a1ace58f064fa3d8312d5d247f52)

--- prebuilt/app/Chromium/Android.mk
+++ prebuilt/app/Chromium/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := Chromium
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES    := 32bit/ChromePublic.apk
+LOCAL_SRC_FILES    := 32bit/MonochromePublic.apk
 LOCAL_CERTIFICATE  := PRESIGNED
 LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
 include $(BUILD_PREBUILT)
