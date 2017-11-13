58270a3a105fe79da0f1bf84736ba02f5807ca35
Gabriele M <moto.falcon.git@gmail.com>
2017-11-13 23:15:29 +0100

    extract_utils: Fix pinning when not cleaning vendor dir
    Skip the extraction of pinned files if the ones currently available
    have the expected sha1. If we don't, we will overwrite pinned files
    with potentially incorrect files when the current vendor files are
    not moved to a temporary directory (i.e. when not cleaning vendor).
    
    Change-Id: I640d6bf2ed98eb366a4df17f0ebeaec81cb5274b

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -967,7 +967,33 @@ function extract() {
         fi
         local DEST="$OUTPUT_DIR/$FROM"
 
-        if [ "$SRC" = "adb" ]; then
+        # Check pinned files
+        local HASH="${HASHLIST[$i-1]}"
+        local KEEP=""
+        if [ "$DISABLE_PINNING" != "1" ] && [ ! -z "$HASH" ] && [ "$HASH" != "x" ]; then
+            if [ -f "$DEST" ]; then
+                local PINNED="$DEST"
+            else
+                local PINNED="$TMP_DIR/$FROM"
+            fi
+            if [ -f "$PINNED" ]; then
+                if [ "$(uname)" == "Darwin" ]; then
+                    local TMP_HASH=$(shasum "$PINNED" | awk '{print $1}' )
+                else
+                    local TMP_HASH=$(sha1sum "$PINNED" | awk '{print $1}' )
+                fi
+                if [ "$TMP_HASH" = "$HASH" ]; then
+                    KEEP="1"
+                    if [ ! -f "$DEST" ]; then
+                        cp -p "$PINNED" "$DEST"
+                    fi
+                fi
+            fi
+        fi
+
+        if [ "$KEEP" = "1" ]; then
+            printf '    + (keeping pinned file with hash %s)\n' "$HASH"
+        elif [ "$SRC" = "adb" ]; then
             # Try CM target first
             adb pull "/$TARGET" "$DEST"
             # if file does not exist try OEM target
