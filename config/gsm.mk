322c8d201a89f0914aaba57e844698e8a9a7d192
maxwen <max.weninger@gmail.com>
2018-01-03 13:07:58 +0100

    omni: remove prebuilt spn xml file
    Change-Id: I3262797a51bc2c14dc05c878e9fd39051587462e

--- config/gsm.mk
+++ config/gsm.mk
@@ -2,9 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
