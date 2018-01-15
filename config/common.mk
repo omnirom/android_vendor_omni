1e301cc41d88255f79bfb05cb08dbc8f04d6f499
mTresk <djtresk@gmail.com>
2018-01-15 08:45:26 +0500

    Change defualt ringtones

--- config/common.mk
+++ config/common.mk
@@ -51,23 +51,9 @@ PRODUCT_COPY_FILES += \
 
 # custom omni sounds
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=omni_ringtone1.ogg \
-    ro.config.notification_sound=omni_notification1.ogg \
-    ro.config.alarm_alert=omni_alarm1.ogg
-
-PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/sounds/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/omni/prebuilt/sounds/VideoRecord_48k.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/omni/prebuilt/sounds/VideoStop_48k.ogg:system/media/audio/ui/VideoStop.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone1.ogg:system/media/audio/ringtones/omni_ringtone1.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone2.ogg:system/media/audio/ringtones/omni_ringtone2.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone3.ogg:system/media/audio/ringtones/omni_ringtone3.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm1.ogg:system/media/audio/alarms/omni_alarm1.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm2.ogg:system/media/audio/alarms/omni_alarm2.ogg \
-    vendor/omni/prebuilt/sounds/omni_notification1.ogg:system/media/audio/notifications/omni_notification1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg
+    ro.config.ringtone=The_big_adventure.ogg \
+    ro.config.notification_sound=Ariel.ogg \
+    ro.config.alarm_alert=Argon.ogg
 
 # mkshrc
 PRODUCT_COPY_FILES += \
