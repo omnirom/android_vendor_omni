8f25ac8518ff9398c854c5beede71310e5aa8a83
Menno van Grinsven <scanno71@gmail.com>
2018-01-26 09:14:49 +0100

    omni: Lets try to get MonoChromePublic working as Webview implementation
    Change-Id: I8f8508068b4bc521a38402b3ade7d8b66debc2e1

--- prebuilt/app/Chromium/Android.mk
+++ prebuilt/app/Chromium/Android.mk
@@ -6,7 +6,19 @@ LOCAL_MODULE       := Chromium
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_SRC_FILES    := 32bit/MonochromePublic.apk
-LOCAL_CERTIFICATE  := PRESIGNED
+# LOCAL_CERTIFICATE  := PRESIGNED
 LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
+
+#LOCAL_MODULE_TARGET_ARCH := arm arm64 mips x86 x86_64
+#my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+#LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
+
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libmonochrome.so
+
 include $(BUILD_PREBUILT)
 endif
