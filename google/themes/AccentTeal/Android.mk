502f5d9ce716c3b69a75ec2b98014734f3e48c8b
Marko Man <darkobas@gmail.com>
2018-03-02 22:58:13 +0100

    WIP: Poor Man themes: Bananasplit
    Change-Id: I811d28057a47166d014ca62c1c8f549213de8184

---
+++ google/themes/AccentTeal/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := AccentTeal
+LOCAL_RRO_THEME := AccentTeal
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+include $(BUILD_RRO_PACKAGE)
