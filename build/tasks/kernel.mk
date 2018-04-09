3eadd288171bf9dbdf5e419f1cbc22b0a92bece0
mTresk <djtresk@gmail.com>
2018-04-09 06:17:00 +0200

    Build kernel with Clang 7.0.1

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -17,7 +17,6 @@
 # Android makefile to build kernel as a part of Android Build
 #
 # Configuration
-# =============
 #
 # These config vars are usually set in BoardConfig.mk:
 #
@@ -236,7 +235,7 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
          KERNEL_CLANG_VERSION := $(shell find $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/ -name AndroidVersion.txt -exec grep -l $(TARGET_KERNEL_CLANG_VERSION) "{}" \; | sed -e 's|/AndroidVersion.txt$$||g;s|^.*/||g')
      else
          # Only set the latest version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
-         KERNEL_CLANG_VERSION := clang-4667116
+         KERNEL_CLANG_VERSION := clang-4679922
      endif
      TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
      ifeq ($(KERNEL_ARCH),arm64)
