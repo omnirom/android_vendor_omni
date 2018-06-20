fb01aaa34b4ebe1b2480864b6da8ce6dcaa0d961
AnilKumar Chimata <anilc@codeaurora.org>
2018-06-20 20:25:55 +0530

    soong_config: Add new flags for HW FDE
    Add new flags for HW FDE to pass CFlags to source code.
    
    CRs-Fixed: 2210986
    Change-Id: I70d68f015ee8eb8d123ce863ce8e36d0310bc082

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,14 @@ type Product_variables struct {
 	Healthd_use_battery_info struct {
 		Cflags []string
 	}
+	Supports_hw_fde struct {
+		Cflags []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Supports_hw_fde_perf struct {
+		Cflags []string
+	}
 	TargetNeedsHWCOnFirstRef struct {
 		Cflags []string
 	}
@@ -13,6 +21,8 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Healthd_use_battery_info  *bool `json:",omitempty"`
+	Supports_hw_fde  *bool `json:",omitempty"`
+	Supports_hw_fde_perf  *bool `json:",omitempty"`
 	TargetNeedsHWCOnFirstRef  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Target_use_sdclang        *bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,8 @@ _contents := $(_contents)    "Omnirom": {$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
+$(call add_json_bool, Supports_hw_fde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Supports_hw_fde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
 $(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
 $(call add_json_bool, Target_use_sdclang,       $(filter true,$(TARGET_USE_SDCLANG)))
