cbc3db84e1ed549a97bade286b3ab595ab6c5ae1
maxwen <max.weninger@gmail.com>
2018-08-12 17:01:57 +0200

    omni: move to PRODUCT_SYSTEM_DEFAULT_PROPERTIES
    Change-Id: I76c0a4a3b6c4572f1b0197c4fc39151640ef9de1

--- config/phone-xxhdpi-4096-dalvik-heap.mk
+++ config/phone-xxhdpi-4096-dalvik-heap.mk
@@ -16,7 +16,7 @@
 
 # Provides overrides to configure the Dalvik heap for a 3G phone
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
