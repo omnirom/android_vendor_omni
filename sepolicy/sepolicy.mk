50e6b453475b92e027580d221ed42294ab8ab1e5
maxwen <max.weninger@gmail.com>
2018-08-20 11:15:56 +0200

    omni: remove old sepolicy cruft
    lets start new
    
    Change-Id: I3389451bfd56a5453d4fd8b2d291e6620ee2e529

--- sepolicy/sepolicy.mk
+++
@@ -1,9 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# inherit from Omni
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/omni/sepolicy/common
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omni/sepolicy/private
