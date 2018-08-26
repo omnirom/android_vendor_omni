6d12b45c73b2541a893e704ae4dde4e9fc36182b
Marko Man <darkobas@gmail.com>
2018-08-26 23:15:26 +0200

    vendor sepolicy genesis v2
    Change-Id: I1ccf1ffed326c1ae7d008a847b11b58227b7e7e3

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Omni
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/omni/sepolicy/common
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omni/sepolicy/private
