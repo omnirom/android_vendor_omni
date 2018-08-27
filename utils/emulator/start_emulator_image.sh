88a9ef0e10897545b0807d82a729ba2263abd70a
maxwen <max.weninger@gmail.com>
2018-08-27 21:28:33 +0200

    omni: switch emulator to pixel_2_xl
    Change-Id: Ife658a7d2e67ecf39c1f2f06262f4639fd582a3c

--- utils/emulator/start_emulator_image.sh
+++ utils/emulator/start_emulator_image.sh
@@ -3,4 +3,4 @@
 export ANDROID_BUILD_TOP=`pwd`
 export ANDROID_PRODUCT_OUT=`pwd`
 
-/home/maxl/Android/Sdk/emulator/emulator  -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_xl -gpu host -writable-system -qemu -cpu qemu64
+/home/maxl/Android/Sdk/emulator/emulator  -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_2_xl -gpu host -writable-system -qemu -cpu qemu64
