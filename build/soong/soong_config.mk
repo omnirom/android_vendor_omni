7b4c6b503bc464bdcd877233e22677ece3307011
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-28 18:22:25 -0600

    soong_config: Add add_json_str_omitempty function
    * This function wraps add_json_str so that it does not add the soong
      variable key:value pair to json structure if the second argument
      (value) is empty/unset, preventing the need for more complex
      conditional logic in the middle the code that builds the struct
    * This concept allows us to make use of the omitempty field option
      for purposes like single variables controlling ifdef blocks as well
      as carrying the desired value
    
    Change-Id: I99c8c162069c2aa8ff3d0bab2636b01181e74a9d

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,9 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+
 _contents := $(_contents)    "Omnirom":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Avoid_calling_into_HWC_onFirstRef,                $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
+$(call add_json_bool, Avoid_calling_into_HWC_onFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
