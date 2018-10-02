bd474f306d4eb933992cc618082a1849843ff7ce
maxwen <max.weninger@gmail.com>
2018-10-02 12:17:26 +0200

    loco poco
    Change-Id: I78a506c2372b4e93cc20212e7f0dbcebcb0aa328

--- Android.mk
+++ Android.mk
@@ -25,4 +25,6 @@ LOCAL_PATH := $(call my-dir)
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
+# loco
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
