18f3b0c14dba2e1d5a8697f7a733d650b12a63bf
micky387 <mickaelsaibi@free.fr>
2018-10-15 21:14:01 +0200

    [1/2] vendor: add Huawei fast charge option
    Change-Id: Iacfedbbc0d67df03a5a55036ded09c1b5194867a

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Healthd_enable_huawei_fastchg struct {
+		Cflags []string
+	}
 	Healthd_use_battery_info struct {
 		Cflags []string
 	}
@@ -9,6 +12,7 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Healthd_enable_huawei_fastchg  *bool `json:",omitempty"`
 	Healthd_use_battery_info  *bool `json:",omitempty"`
 	TargetNeedsHWCOnFirstRef  *bool `json:",omitempty"`
 }
