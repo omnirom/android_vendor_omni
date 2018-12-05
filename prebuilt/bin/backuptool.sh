0ae52985e6127112536ccfe6173d08c6e4803274
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:21 +0100

    omni: Support system-as-root A-only backuptool
    Change-Id: I8336e5ab3c9d03fae7426044066318b987653d99

--- prebuilt/bin/backuptool.sh
+++ prebuilt/bin/backuptool.sh
@@ -4,7 +4,7 @@
 #
 
 export C=/tmp/backupdir
-export S=/system
+export S=$2
 export V=9
 
 export ADDOND_VERSION=1
@@ -13,9 +13,9 @@ DEBUG=0
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
-  rm -rf /tmp/addon.d/
-  mkdir -p /tmp/addon.d/
-  cp -a /system/addon.d/* /tmp/addon.d/
+  if [ -d $S/addon.d/ ]; then
+    mkdir -p /tmp/addon.d/
+    cp -a $S/addon.d/* /tmp/addon.d/
     # Discard any scripts that aren't at least our version level
     for f in /postinstall/tmp/addon.d/*sh; do
       SCRIPT_VERSION=$(grep "^# ADDOND_VERSION=" $f | cut -d= -f2)
@@ -26,35 +26,40 @@ preserve_addon_d() {
         rm $f
       fi
     done
-  chmod 755 /tmp/addon.d/*.sh
+    chmod 755 /tmp/addon.d/*.sh
+  fi
 }
 
 # Restore /system/addon.d in /tmp/addon.d
 restore_addon_d() {
-  cp -a /tmp/addon.d/* /system/addon.d/
-  rm -rf /tmp/addon.d/
+  if [ -d /tmp/addon.d/ ]; then
+    mkdir -p $S/addon.d/
+    cp -a /tmp/addon.d/* $S/addon.d/
+    rm -rf /tmp/addon.d/
+  fi
 }
 
 # Restore only if backup has the expected major and minor version
 check_prereq() {
-  if [ ! -f /tmp/build.prop ]; then
+  if [ ! -f $S/build.prop ]; then
     # this will block any backups made before 8 cause file was not copied before
     echo "Not restoring files from incompatible version: $V"
     exit 127
   fi
-  if ( ! grep -q "^ro.build.version.release=$V.*" /tmp/build.prop ); then
+  if ( ! grep -q "^ro.build.version.release=$V.*" $S/build.prop ); then
     echo "Not restoring files from incompatible version: $V"
     exit 127
   fi
 }
 
 check_blacklist() {
-  if [ -f /system/addon.d/blacklist ];then
+  if [ -f $S/addon.d/blacklist -a -d /$1/addon.d/ ]; then
     ## Discard any known bad backup scripts
     cd /$1/addon.d/
     for f in *sh; do
+      [ -f $f ] || continue
       s=$(md5sum $f | awk {'print $1'})
-      grep -q $s /system/addon.d/blacklist && rm -f $f
+      grep -q $s $S/addon.d/blacklist && rm -f $f
     done
   fi
 }
