887d171b2bfd82a010f3fc242e05c75a9318fa3a
maxwen <max.weninger@gmail.com>
2018-12-15 13:27:15 +0100

    omni: fix using omni charger sound
    no need for messing with any overlay - simply put the file
    at the place its expected by default
    
    Change-Id: I2d9f7307fc03ca30053f70bcee10e98e6ba11ef3

--- config/common.mk
+++ config/common.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/sounds/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
     vendor/omni/prebuilt/sounds/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
     vendor/omni/prebuilt/sounds/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_charge_start.ogg:system/media/audio/ui/omni_charge_start.ogg
+    vendor/omni/prebuilt/sounds/omni_charge_start.ogg:system/media/audio/ui/ChargingStarted.ogg
 
 # mkshrc
 PRODUCT_COPY_FILES += \
