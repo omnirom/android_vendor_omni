2d85b4faa229265ecd4aa745ae6f4ab973dba60a
oshmoun <oshmoun100@googlemail.com>
2019-01-18 02:19:45 +0100

    version: remove omni fingerprint
    the fingerprint was used to signal the need to update packages.
    remove it now that it has been replaced with ro.build.date
    
    Change-Id: Iab770926079049d55e637e35f0559a1d09539e55

--- config/version.mk
+++ config/version.mk
@@ -22,7 +22,3 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.modversion=OmniROM-$(ROM_VERSION) \
     ro.omni.version=$(ROM_VERSION)
-
-ROM_FINGERPRINT := OmniROM/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d.%H:%M)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.omni.fingerprint=$(ROM_FINGERPRINT)
