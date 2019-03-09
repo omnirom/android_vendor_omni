96380903134ade86b274c07f39a87abc9ea91298
Erfan Abdi <erfangplus@gmail.com>
2019-03-09 12:03:41 +0200

    soong_config: Add flag for devices use metadata as FDE key
    Change-Id: I5f70280e72613a8f850cb3263c45e95e80cb8b3c
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,6 +5,7 @@ _contents := $(_contents)    "Omnirom": {$(newline)
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
 $(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
+$(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
