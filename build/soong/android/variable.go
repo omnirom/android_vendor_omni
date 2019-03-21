0fd4fe9d34c61ef503102aff4e24f9cc8a91ba61
Marko Man <darkobas@gmail.com>
2019-03-21 22:20:10 +0100

    vendor: add TARGET_NEEDS_HWC_ONFIRSTREF option
    Change-Id: I1cb20129af69f641107c2b552cfd69eecfe65d64

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,10 @@
 package android
 type Product_variables struct {
+	Avoid_calling_into_HWC_onFirstRef struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
+	Avoid_calling_into_HWC_onFirstRef  *bool `json:",omitempty"`
 }
