83cdc3d7b04fffe60286e4292af7ea3b6f51e8b5
Marko Man <darkobas@gmail.com>
2019-03-21 22:20:10 +0100

    vendor: add TARGET_NEEDS_HWC_ONFIRSTREF option
    Change-Id: I1cb20129af69f641107c2b552cfd69eecfe65d64

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,7 @@
-_contents := $(_contents)    "Omnirom":{$(newline)
+_contents := $(_contents)    "Omnirom": {$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
