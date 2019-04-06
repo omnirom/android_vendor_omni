021c61d2655e290af8d83beee00b3abdacc8bce1
Stefan Berger <s.berger81@gmail.com>
2019-04-06 10:59:29 +0200

    vendor: add HEALTHD_USE_BATTERY_INFO option
    Change-Id: Ifdf879fe1cb5735c21d9528208da29ed9cfe116b

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,6 +3,7 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _contents := $(_contents)    "Omnirom": {$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
 $(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
