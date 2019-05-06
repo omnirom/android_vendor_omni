d1849e99ca2b3b5ae85b20da0e387219d76e14e4
Marko Man <darkobas@gmail.com>
2019-05-06 09:07:42 +0200

    vendor: add exfat and ntfs perms for vold
    Change-Id: Ia350d6373f13de4dfab9c2fc6456d8d53de554a9

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,3 +7,4 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/omni/sepolicy/common
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omni/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += vendor/omni/sepolicy/public
