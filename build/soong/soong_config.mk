1d8d7911157dada7294910213fa7780fe16374f7
frap129 <joe@maples.dev>
2019-06-12 15:03:53 +0200

    soong: add sdclang
    Change-Id: Ib2b955f76ed15934b11fa2f7b58cf71ea772077a

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,8 @@ _contents := $(_contents)    "Omnirom": {$(newline)
 $(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
 $(call add_json_bool, TargetNeedsHWCOnFirstRef, $(filter true,$(TARGET_NEEDS_HWC_ONFIRSTREF)))
 $(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
+$(call add_json_bool, Target_use_sdclang,       $(filter true,$(TARGET_USE_SDCLANG)))
+
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
