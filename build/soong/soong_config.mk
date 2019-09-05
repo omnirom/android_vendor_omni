46b46a1bc92722a8837f2d580758e41bf3cbd6a2
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    lineage: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Omnirom": {$(newline)
+_json_contents := $(_json_contents)    "Omnirom":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Healthd_use_battery_info, $(filter true,$(HEALTHD_USE_BATTERY_INFO)))
@@ -10,6 +10,6 @@ $(call add_json_bool, Target_use_sdclang,       $(filter true,$(TARGET_USE_SDCLA
 
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
