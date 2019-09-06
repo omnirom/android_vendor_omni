4716bc37d11ff18caef72ad9c79f7290517706cb
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 10:44:50 -0700

    kernel: Use prebuilt binaries
    perl, lz4c, and a bunch of other binaries are not allowed anymore.
    So we need to provide our own.
    
    Change-Id: Idbc88b76247b3dda7c5d52e5eed2d548c33f6f2d

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -232,6 +232,10 @@ ifneq ($(KERNEL_NO_GCC), true)
     PATH_OVERRIDE += PATH=$(KERNEL_TOOLCHAIN_PATH_gcc):$$PATH
 endif
 
+
+# System tools are no longer allowed on 10+
+PATH_OVERRIDE += $(TOOLS_PATH_OVERRIDE)
+
 ifeq (true,$(filter true, $(TARGET_NEEDS_DTBOIMAGE) $(BOARD_KERNEL_SEPARATED_DTBO)))
     KERNEL_MAKE_FLAGS += DTC_EXT=$(KERNEL_BUILD_OUT_PREFIX)$(DTC)
 endif
