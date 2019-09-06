0fd9789656cff3ca4be4625789405671a89cf650
maxwen <max.weninger@gmail.com>
2019-09-07 00:57:51 +0200

    omni: make emulator work on Q
    Change-Id: I4fe6e0ed2604acdec56bc1851f5053e2373bbfc5

--- utils/emulator/start_emulator_image.sh
+++ utils/emulator/start_emulator_image.sh
@@ -3,5 +3,5 @@
 export ANDROID_BUILD_TOP=`pwd`
 export ANDROID_PRODUCT_OUT=`pwd`
 
-cp $HOME/Android/Sdk/system-images/android-28/google_apis/x86/vendor.img .
-$HOME/Android/Sdk/emulator/emulator  -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_2_xl -gpu host -writable-system -qemu -cpu qemu64
+cp $HOME/Android/Sdk/system-images/android-29/google_apis/x86/vendor.img .
+$HOME/Android/Sdk/emulator/emulator  -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_2_xl -gpu host -writable-system -qemu -append "androidboot.vbmeta.size=4352 androidboot.vbmeta.hash_alg=sha256 androidboot.vbmeta.digest=8fe6dcf73940be6e49f5b466d1d8c0dfe993873aca2dd0358fd63a0eb1650693"
