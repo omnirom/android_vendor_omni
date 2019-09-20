b65c4db3c10718e54088139e5c762b5281436998
Marko Man <darkobas@gmail.com>
2019-09-20 10:50:07 +0200

    vendor: soong: move op fastcharge and vendor init here
    Change-Id: I0c6e1164e0998a883fe1ba04a06bc2ae4cd62b3b

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,17 +3,27 @@ type Product_variables struct {
 	Healthd_use_battery_info struct {
 		Cflags []string
 	}
+        Healthd_enable_op_fastchg struct {
+                Cflags []string
+        }
+
 	TargetNeedsHWCOnFirstRef struct {
 		Cflags []string
 	}
 	Uses_metadata_as_fde_key struct {
 		Cflags []string
 	}
+        Target_init_vendor_lib struct {
+                Cflags []string
+        }
+
 }
 
 type ProductVariables struct {
-	Healthd_use_battery_info  *bool `json:",omitempty"`
-	TargetNeedsHWCOnFirstRef  *bool `json:",omitempty"`
-	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
-	Target_use_sdclang        *bool `json:",omitempty"`
+	Healthd_use_battery_info   *bool `json:",omitempty"`
+	Healthd_enable_op_fastchg  *bool `json:",omitempty"`
+	TargetNeedsHWCOnFirstRef   *bool `json:",omitempty"`
+	Uses_metadata_as_fde_key   *bool `json:",omitempty"`
+	Target_use_sdclang         *bool `json:",omitempty"`
+	Target_init_vendor_lib     *bool `json:",omitempty"`
 }
