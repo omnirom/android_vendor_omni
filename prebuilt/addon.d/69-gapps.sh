3b38457d9b509bc0ee11ed5333ac23bcca59cabf
Marko Man <darkobas@gmail.com>
2019-10-11 13:23:16 +0200

    vendor: fix the fix backup script
    this is exactly why we started this thing
    
    Change-Id: Iaa75916b0719daa3604fe2da6ed41d8ae57686ef

--- prebuilt/addon.d/69-gapps.sh
+++ prebuilt/addon.d/69-gapps.sh
@@ -47,7 +47,6 @@ priv-app/GoogleBackupTransport/GoogleBackupTransport.apk
 priv-app/GoogleExtServices/GoogleExtServices.apk
 priv-app/GoogleFeedback/GoogleFeedback.apk
 priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
-priv-app/GooglePackageInstaller/GooglePackageInstaller.apk
 priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
 priv-app/GoogleRestore/GoogleRestore.apk
 priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
@@ -141,9 +140,6 @@ case "$1" in
     # Remove/postinstall Stock/AOSP apps (from GApps Installer)
     rm -rf /postinstall/system/priv-app/ExtServices
     rm -rf /postinstall/system/app/ExtShared
-    rm -rf /postinstall/system/app/PackageInstaller
-    rm -rf /postinstall/system/priv-app/PackageInstaller
-    rm -rf /postinstall/system/priv-app/packageinstaller
     rm -rf /postinstall/system/app/Provision
     rm -rf /postinstall/system/priv-app/Provision
 
