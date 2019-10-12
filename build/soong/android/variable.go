41aabc5891ae0c3726890cad9290db81adff3241
Marko Man <darkobas@gmail.com>
2019-10-13 00:43:56 +0200

    vendor: change vendor init soong config
    Change-Id: Ib0e768813a0b367f6df9801821347040fd5f161a

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -13,7 +13,7 @@ type Product_variables struct {
 		Cflags []string
 	}
         Target_init_vendor_lib struct {
-                Whole_static_libs []string
+                Static_libs []string
                 Cflags []string
         }
         Target_camera_needs_client_info struct {
