d63493ba277349cd8806e60510c141150e0af0b1
El Dainosor <eldainosor@gmail.com>
2019-10-28 15:26:07 -0400

    statix: initial overlay stub
    Change-Id: I4e8325d8007a8349d8933e4b8e9a38c2a876cc17

--- config/packages.mk
+++ config/packages.mk
@@ -16,6 +16,9 @@ PRODUCT_PACKAGES += \
     ThemePicker \
     webview
 
+PRODUCT_PACKAGES += \
+     StatixOverlayStub
+
 # Additional tools
 PRODUCT_PACKAGES += \
     e2fsck \
@@ -56,64 +59,6 @@ endif
 # Telephony extension
 PRODUCT_PACKAGES += telephony-ext
 PRODUCT_BOOT_JARS += telephony-ext
-
-# Themes
-#PRODUCT_PACKAGES += \
-    GboardOmniTheme
-
-#PRODUCT_PACKAGES += \
-    ContactsTheme \
-    DocumentsUITheme \
-    DialerTheme \
-    MessagesTheme \
-    MusicFXTheme \
-    TelecommTheme \
-    PhonographTheme
-
-#PRODUCT_PACKAGES += \
-    NotificationsDark \
-    NotificationsLight \
-    NotificationsPrimary
-
-#PRODUCT_PACKAGES += \
-    AccentSluttyPink \
-    AccentPixel \
-    AccentGoldenShower \
-    AccentDeepOrange \
-    AccentOmni \
-    AccentWhite \
-    AccentTeal \
-    AccentFromHell \
-    AccentBlueMonday \
-    AccentSmokingGreen \
-    AccentDeadRed \
-    AccentRottenOrange \
-    AccentDeepPurple
-
-#PRODUCT_PACKAGES += \
-    PrimaryAlmostBlack \
-    PrimaryBlack \
-    PrimaryOmni \
-    PrimaryWhite \
-    PrimaryColdWhite \
-    PrimaryWarmWhite \
-    PrimaryDarkBlue \
-    PrimaryViolator \
-    PrimaryTealMeal
-
-# Textclassifiers
-#PRODUCT_PACKAGES += \
-    textclassifier.langid.model \
-    textclassifier.smartselection.bundle1 \
-    textclassifier.smartselection.ar.model \
-    textclassifier.smartselection.it.model \
-    textclassifier.smartselection.nl.model \
-    textclassifier.smartselection.pl.model \
-    textclassifier.smartselection.pt.model \
-    textclassifier.smartselection.ru.model \
-    textclassifier.smartselection.tr.model \
-    textclassifier.smartselection.zh.model \
-    textclassifier.smartselection.zh-Hant.model
 
 # for fun
 PRODUCT_PACKAGES += \
