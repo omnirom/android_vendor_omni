394eae42548c00f85de11352a835ec114c27a76e
maxwen <max.weninger@gmail.com>
2019-10-31 21:14:39 +0100

    omni: omnify the overlay stub
    just add a simple omni theme for now
    
    Change-Id: I3e5c4c36253f2a8cf8a74e493e5d269a70a4c40a

--- overlaystub/Android.mk
+++ overlaystub/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := StatixOverlayStub
+LOCAL_PACKAGE_NAME := OmniOverlayStub
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
