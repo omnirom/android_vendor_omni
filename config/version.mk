0d3bab95fb9d10bee0c08837b9fb448142ddb6e9
Marko Man <darkobas@gmail.com>
2019-11-01 17:59:44 +0100

    vendor: enable optional gapps build
    to use: clone gapps repo to vendor/gapps
    export BUILDTYPE_RELEASE=GAPPS
    
    Change-Id: I88f9d707440f54ce4965e9213469e1aa13735334

--- config/version.mk
+++ config/version.mk
@@ -8,8 +8,9 @@ TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst omni_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
-ifdef BUILDTYPE_RELEASE
-    ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+ifeq ($(ROM_BUILDTYPE),GAPPS)
+include vendor/gapps/config.mk
+    ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 else
 ifeq ($(ROM_BUILDTIME_UTC),y)
     ROM_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
