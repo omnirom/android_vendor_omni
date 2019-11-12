2b26292c480678f33b829614b8b34cb94a632cff
isadore belle <isa@gerrit.omnirom.org>
2019-11-12 22:49:52 +0100

    Revert "vendor: remove empty space :)"
    This reverts commit cb21dbda094ada45539d2855a848a0dbf750fd86.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Ie563bf2c1a29968a773958f69a8eceee09bfde7b

--- build/core/qcom_utils.mk
+++ build/core/qcom_utils.mk
@@ -37,8 +37,10 @@ QSD8K_BOARD_PLATFORMS := qsd8k
 
 
 # vars for use by utils
-colon := $(empty):$(empty)
-underscore := $(empty)_$(empty)
+#empty :=
+#space := $(empty) $(empty)
+#colon := $(empty):$(empty)
+#underscore := $(empty)_$(empty)
 
 # $(call match-word,w1,w2)
 # checks if w1 == w2
