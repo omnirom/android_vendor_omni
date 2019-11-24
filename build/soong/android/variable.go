2d97b19366ae23de86a3ae0fa01529e05b2d28ea
maxwen <max.weninger@gmail.com>
2019-11-24 23:39:33 +0100

    omni: soong: add TARGET_ENFORCE_AB_OTA_PARTITION_LIST variable
    Change-Id: Iadb506093872fae535189a01058b66966e6a0fcd

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,36 +1,40 @@
 package android
 type Product_variables struct {
-	Healthd_use_battery_info struct {
-		Cflags []string
-	}
-        Healthd_enable_op_fastchg struct {
-                Cflags []string
-        }
-	TargetNeedsHWCOnFirstRef struct {
-		Cflags []string
-	}
-	Uses_metadata_as_fde_key struct {
-		Cflags []string
-	}
-        Target_init_vendor_lib struct {
-                Static_libs []string
-                Cflags []string
-        }
-        Target_camera_needs_client_info struct {
-                Cflags []string
-        }
-        Target_motorized_camera struct {
-                Cflags []string
-        }
+    Healthd_use_battery_info struct {
+        Cflags []string
+    }
+    Healthd_enable_op_fastchg struct {
+        Cflags []string
+    }
+    TargetNeedsHWCOnFirstRef struct {
+        Cflags []string
+    }
+    Uses_metadata_as_fde_key struct {
+        Cflags []string
+    }
+    Target_init_vendor_lib struct {
+        Static_libs []string
+        Cflags []string
+    }
+    Target_camera_needs_client_info struct {
+        Cflags []string
+    }
+    Target_motorized_camera struct {
+        Cflags []string
+    }
+    Target_enforce_ab_ota_partition_list struct {
+        Cflags []string
+    }
 }
 
 type ProductVariables struct {
-	Healthd_use_battery_info                *bool `json:",omitempty"`
-	Healthd_enable_op_fastchg               *bool `json:",omitempty"`
-	TargetNeedsHWCOnFirstRef                *bool `json:",omitempty"`
-	Uses_metadata_as_fde_key                *bool `json:",omitempty"`
-	Target_use_sdclang                      *bool `json:",omitempty"`
-	Target_camera_needs_client_info         *bool `json:",omitempty"`
-	Target_motorized_camera                 *bool `json:",omitempty"`
-	Target_init_vendor_lib                  *string `json:",omitempty"`
+    Healthd_use_battery_info                *bool `json:",omitempty"`
+    Healthd_enable_op_fastchg               *bool `json:",omitempty"`
+    TargetNeedsHWCOnFirstRef                *bool `json:",omitempty"`
+    Uses_metadata_as_fde_key                *bool `json:",omitempty"`
+    Target_use_sdclang                      *bool `json:",omitempty"`
+    Target_camera_needs_client_info         *bool `json:",omitempty"`
+    Target_motorized_camera                 *bool `json:",omitempty"`
+    Target_init_vendor_lib                  *string `json:",omitempty"`
+    Target_enforce_ab_ota_partition_list    *bool `json:",omitempty"`
 }
