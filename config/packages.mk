3a1490177fc7354d89d198a4df87768e66ef5d01
maxwen <max.weninger@gmail.com>
2019-12-07 12:10:02 +0100

    omni: add vnc server integration bits
    Change-Id: Ic9a52c28ddc0bba4ae519d4f30de1adb6f127496

--- config/packages.mk
+++ config/packages.mk
@@ -34,7 +34,10 @@ PRODUCT_PACKAGES += \
     mkshrc_vendor \
     toybox_vendor \
     sh_vendor \
-    vim
+    vim \
+    vncflinger \
+    vncpasswd \
+    OmniRemote
 
 ifeq ($(BOARD_INCLUDE_CMDLINE_TOOLS),true)
 PRODUCT_PACKAGES += \
