0ab0e110fbcaa388cfb2639a60c1192ca89b7b30
maxwen <max.weninger@gmail.com>
2020-06-11 13:58:26 +0200

    omni: add prebuilt store installer apk
    Change-Id: I0940faf1f673fffe9526b63bf1e6f3f4598ecdaf

--- prebuilt/app/OmniStore/Android.mk
+++ prebuilt/app/OmniStore/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := EggGame
-LOCAL_SRC_FILES := EggGame.apk
+LOCAL_MODULE := OmniStoreInstaller
+LOCAL_SRC_FILES := OmniStoreInstaller.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
