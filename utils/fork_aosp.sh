0ce74d6c55a0a8db16504e66ff8e3cbc2c8f4d9c
micky387 <mickaelsaibi@free.fr>
2020-10-01 03:31:52 +0200

    omni: A11 now
    Change-Id: I1f99cb66299d0771c3cbb5dad2f9b7da79321493

--- utils/fork_aosp.sh
+++ utils/fork_aosp.sh
@@ -4,8 +4,8 @@
 
 # Configuration
 PREFIX=android_
-BRANCH=android-10
-SOURCE=android-10.0.0_r<INSERT_CORRECT_TAG>
+BRANCH=android-11
+SOURCE=android-11.0.0_r<INSERT_CORRECT_TAG>
 USERNAME=<INSERT USER>
 MANIFEST=android/default.xml
 REMOVE=android/remove.xml
