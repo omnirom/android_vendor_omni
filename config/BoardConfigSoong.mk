7d3ac7a7ceb7b01b2837fd1effdaf649fa934f75
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: Add omni soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -26,3 +26,41 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += omniGlobalVars
+SOONG_CONFIG_omniGlobalVars += \
+    target_init_vendor_lib \
+    target_surfaceflinger_fod_lib \
+    healthd_use_battery_info \
+    healthd_enable_op_fastchg \
+    targetNeedsHWCOnFirstRef \
+    uses_metadata_as_fde_key \
+    target_use_sdclang \
+    target_camera_needs_client_info \
+    target_enforce_ab_ota_partition_list \
+    target_vold_vendor_lib
+
+SOONG_CONFIG_NAMESPACES += omniQcomVars
+SOONG_CONFIG_omniQcomVars += \
+    healthd_enable_tricolor_led \
+    supports_hw_fde \
+    supports_hw_fde_perf \
+
+# Soong bool variables
+SOONG_CONFIG_omniQcomVars_healthd_enable_tricolor_led := $(HEALTHD_ENABLE_TRICOLOR_LED)
+SOONG_CONFIG_omniQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
+SOONG_CONFIG_omniQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
+SOONG_CONFIG_omniGlobalVars_healthd_use_battery_info := $(HEALTHD_USE_BATTERY_INFO)
+SOONG_CONFIG_omniGlobalVars_healthd_enable_op_fastchg := $(HEALTHD_ENABLE_OP_FASTCHG_CHECK)
+SOONG_CONFIG_omniGlobalVars_targetNeedsHWCOnFirstRef := $(TARGET_NEEDS_HWC_ONFIRSTREF)
+SOONG_CONFIG_omniGlobalVars_uses_metadata_as_fde_key := $(TARGET_USES_METADATA_AS_FDE_KEY)
+SOONG_CONFIG_omniGlobalVars_target_use_sdclang := $(TARGET_USE_SDCLANG)
+SOONG_CONFIG_omniGlobalVars_target_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_omniGlobalVars_target_enforce_ab_ota_partition_list := $(TARGET_ENFORCE_AB_OTA_PARTITION_LIST)
+SOONG_CONFIG_omniGlobalVars_target_vold_vendor_lib := $(TARGET_VOLD_VENDOR_LIB)
+
+# Set default values
+TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong value variables
+SOONG_CONFIG_omniGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
