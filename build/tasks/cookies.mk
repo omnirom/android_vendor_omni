2de2ed0c22f1aea6ef42a6366b0b9e0526d85a16
Julian Veit <Claymore1298@gmail.com>
2021-05-01 11:56:15 +0200

    omni: switch to sha256 checksum
    As md5 is not considered as secure since some time, and sha256 is
    industry standard, lets switch over here too.
    
    Change-Id: I22015be20126a4348affa5ef97e2c28fbd7b4717

--- build/tasks/cookies.mk
+++ build/tasks/cookies.mk
@@ -1,10 +1,10 @@
 
 OMNI_TARGET_PACKAGE := $(PRODUCT_OUT)/omni-$(ROM_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: cookies
 cookies: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(OMNI_TARGET_PACKAGE)
-	$(hide) $(MD5) $(OMNI_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(OMNI_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(OMNI_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(OMNI_TARGET_PACKAGE).sha256sum
 	@echo -e "Package complete: $(OMNI_TARGET_PACKAGE)" >&2
