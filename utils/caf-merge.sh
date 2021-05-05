60dc816f44f2a152c51cdb49f45f5e66e0e72453
micky387 <mickaelsaibi@free.fr>
2021-05-05 23:42:26 +0200

    omni: Add merge --squash into merge script
    Change-Id: I42718264f644a3071b45ae1467168929ebc2c9c4

--- utils/caf-merge.sh
+++ utils/caf-merge.sh
@@ -17,6 +17,11 @@
 echo -e "Enter the CAF ref to merge";
 read ref;
 
+echo -e "Enter the name of the new branch";
+read branch_name;
+
+branch_current="android-11"
+
 cd ../../../
 
 while read path;
@@ -37,6 +42,9 @@ while read path;
     echo " -> repo sync ${path}";
     ret=$(repo sync -d -f --force-sync ${path} 2>&1);
     cd $path;
+    echo " -> creating branch ${branch_name}";
+    ret=$(git checkout -b ${branch_name});
+    ret=$(repo start ${branch_name});
 
     # make sure that environment is clean
     ret=$(git merge --abort 2>&1);
@@ -47,6 +55,11 @@ while read path;
     if echo $ret | grep "CONFLICT (content)" > /dev/null ; then
         echo -e " -> \e[33mWARNING!: \e[31mMERGE CONFLICT\e[0m";
     else
+        echo " -> merging squashed forked commits into ${branch_current} branch";
+        ret=$(git checkout  ${branch_current});
+        ret=$(git merge --squash ${branch_name} 2>&1);
+        ret=$(git add . 2>&1);
+        ret=$(git commit --no-edit 2>&1);
         echo -e " -> \e[32mDONE!\e[0m";
     fi
 
