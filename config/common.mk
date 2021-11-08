42f9becd2f741454309ea0b9d5633868a9564a43
Tony Huang <tonyychuang@google.com>
2021-11-08 20:55:39 +0100

    omni: Add one handed support property for presubmit tests
    Add a System Property flag "ro.support_one_handed_mode" to
    config support one-handed on device or not.
    
    Bug: 154290458
    
    Test: manual
    Test: Presubmit systemUi tests
    Change-Id: I8c8607a5a5e108f92ff975080d0b182aa05c3451

--- config/common.mk
+++ config/common.mk
@@ -113,6 +113,16 @@ PRODUCT_COPY_FILES += \
 # Additional packages
 -include vendor/omni/config/packages.mk
 
+# Set support one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+# Set one_handed_mode screen translate offset percentage
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.debug.one_handed_offset_percentage=50
+# Set one_handed_mode translate animation duration milliseconds
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.debug.one_handed_translate_animation_duration=300
+
 # Versioning
 -include vendor/omni/config/version.mk
 
