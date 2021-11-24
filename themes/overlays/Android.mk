c20bbb6123778bff3524f1010798ac2f7b49fcf9
maxwen <max.weninger@gmail.com>
2021-11-24 01:05:34 +0100

    omni: playing with new overlays
    Change-Id: Ia3f6c7602ed0aae631fef01528f9bcb83228b5b4

--- themes/overlays/Android.mk
+++ themes/overlays/Android.mk
@@ -18,20 +18,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := omni-overlays
 LOCAL_REQUIRED_MODULES := \
 	AccentColorSluttyPinkOverlay \
-	AccentColorBlueMondayOverlay \
 	AccentColorOmniOverlay \
-	AccentColorGoldenShowerOverlay \
-	AccentColorRedDevilOverlay \
-	FontLatoSourceOverlay \
-	FontMuliSourceOverlay \
-	EmptyProductOverlay \
-	IconShapeCircleOverlay
+	AccentColorPurpleOverlay \
+	AccentColorOrangeOverlay
 
 LOCAL_REQUIRED_MODULES += \
-	PrimaryColorOmniBlackOverlay \
+	EmptyProductOverlay
+
+LOCAL_REQUIRED_MODULES += \
+	IconShapeCircleOverlay \
+	IconShapeSquareOverlay \
+	IconShapeSquircleOverlay \
+	IconShapeTeardropOverlay \
+	IconShapePebbleOverlay
+
+LOCAL_REQUIRED_MODULES += \
 	PrimaryColorDarkBlueOverlay \
-	PrimaryColorDeepBlueOverlay \
-	PrimaryColorDarkGreenOverlay
+	PrimaryColorDarkGrayOverlay
 
 include $(BUILD_PHONY_PACKAGE)
 include $(CLEAR_VARS)
