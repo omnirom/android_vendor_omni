04057d3e3654691f7b20502a724288bbba1a630c
Alexander Koskovich <akoskovich@pm.me>
2022-06-16 13:14:14 -0700

    kernel: Move TARGET_KERNEL_ADDITIONAL_FLAGS to kernel task
     * Currently, because DTC_EXT make flag is in the
       kernel task the only way to override it is to
       have TARGET_KERNEL_ADDITIONAL_FLAGS come after.
    
    Change-Id: If62ed979581dc07eafd4628ce2b2d799388d3704

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -20,7 +20,6 @@
 #
 #   TARGET_KERNEL_SOURCE               = Kernel source dir, optional, defaults
 #                                          to kernel/$(TARGET_DEVICE_DIR)
-#   TARGET_KERNEL_ADDITIONAL_FLAGS     = Additional make flags, optional
 #   TARGET_KERNEL_ARCH                 = Kernel Arch
 #   TARGET_KERNEL_CROSS_COMPILE_PREFIX = Compiler prefix (e.g. arm-eabi-)
 #                                          defaults to arm-linux-androidkernel- for arm
@@ -123,10 +122,6 @@ ifeq ($(HOST_OS),darwin)
   KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I$(BUILD_TOP)/external/elfutils/libelf -I/usr/local/opt/openssl/include -fuse-ld=lld" HOSTLDFLAGS="-L/usr/local/opt/openssl/lib -fuse-ld=lld"
 else
   KERNEL_MAKE_FLAGS += HOSTLDFLAGS="-L/usr/lib/x86_64-linux-gnu -L/usr/lib64 -fuse-ld=lld"
-endif
-
-ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
-  KERNEL_MAKE_FLAGS += $(TARGET_KERNEL_ADDITIONAL_FLAGS)
 endif
 
 # Set DTBO image locations so the build system knows to build them
