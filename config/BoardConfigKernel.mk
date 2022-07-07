8442f77e1f71f90e98c57ce7b24637d8f0505544
Chirayu Desai <chirayudesai1@gmail.com>
2022-07-08 03:33:22 +0530

    kernel: Don't print grep error messages when checking version
    Avoids:
    $ lunch lineage_sdk_phone_x86
    grep: build/make/target/board/emulator_x86/Makefile: No such file or directory (x4)
    
    Change-Id: Ie803d5ca93e6e7ae94625e7ee3ab0bf337e0515d
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -75,8 +75,8 @@ else
 endif
 TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(KERNEL_CLANG_VERSION)
 
-KERNEL_VERSION := $(shell grep "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
-KERNEL_PATCHLEVEL := $(shell grep "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_VERSION := $(shell grep -s "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_PATCHLEVEL := $(shell grep -s "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
 TARGET_KERNEL_VERSION ?= $(shell echo $(KERNEL_VERSION)"."$(KERNEL_PATCHLEVEL))
 
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
