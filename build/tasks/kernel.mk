9f96aa4fa4583afe30e1b19c97c5ce4022d5be56
Michael Bestas <mkbestas@lineageos.org>
2022-08-13 15:14:17 +0300

    kernel: Always build dtb targets before dtbo.img
    Fixes:
    error: unable to open output file
    'arch/arm64/boot/dts/google/devices/bluejay/google-base/.gs101-b0.dtb.dts.tmp':
    'No such file or directory'
    
    Change-Id: I457062e46062fb3553421d196b432de7d06a49ad

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -409,8 +409,8 @@ ifeq ($(BOARD_USES_QCOM_MERGE_DTBS_SCRIPT),true)
 else
 	$(hide) find $(DTBO_OUT)/arch/$(KERNEL_ARCH)/boot/dts -type f -name "*.dtbo" | xargs rm -f
 	$(call make-dtbo-target,$(KERNEL_DEFCONFIG))
+	$(call make-dtbo-target,$(TARGET_KERNEL_DTB))
 ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
-	$(call make-dtbo-target,dtbs)
 ifdef BOARD_DTBO_CFG
 	$(MKDTBOIMG) cfg_create $@ $(BOARD_DTBO_CFG) -d $(DTBO_OUT)/arch/$(KERNEL_ARCH)/boot/dts
 else
