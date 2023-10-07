dae8126b10fe20f25f42eb3b675e8db65f87092c
maxwen <max.weninger@gmail.com>
2023-10-07 17:31:23 +0200

    omni: make soong variables follow json syntax
    wrong: CROSS_COMPILE=\\"/data4...
    correct: CROSS_COMPILE=\"/data4...
    
    Change-Id: I5080aef7feb2ec7429cb9ba1f4baccefa61a3cd3

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -18,7 +18,7 @@ SOONG_CONFIG_omniromVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_omniromVarsPlugin += $(1)
-  SOONG_CONFIG_omniromVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_omniromVarsPlugin_$(1) := $($1)
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
