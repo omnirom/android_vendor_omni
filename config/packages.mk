f08587c47c644899f06b95c09ef705e91f848739
maxwen <max.weninger@gmail.com>
2023-11-10 17:14:10 +0100

    omni: add RRO for custom notification action button background
    and cleanup package list
    
    Change-Id: Ifb2c28ffc47cfbf26a3e72afda105ff06cb47d6c

--- config/packages.mk
+++ config/packages.mk
@@ -11,7 +11,9 @@ PRODUCT_PACKAGES += \
     MatLog \
     OmniStoreInstallerPrebuilt \
     OmniOta \
-    OmniWallpaper
+    OmniWallpaper \
+    OmniRemote \
+    OmniControl
 
 # Additional tools
 PRODUCT_PACKAGES += \
@@ -31,47 +33,9 @@ PRODUCT_PACKAGES += \
     vim \
     vncflinger \
     vncpasswd \
-    OmniRemote \
     SystemWebView \
-    OmniControl \
     omni-overlays
 
-#PRODUCT_PACKAGES += \
-    ThemePicker \
-    OmniOverlayStub \
-
-ifeq ($(BOARD_INCLUDE_CMDLINE_TOOLS),true)
-PRODUCT_PACKAGES += \
-    htop \
-    zip
-
-# Openssh
-PRODUCT_PACKAGES += \
-    scp \
-    sftp \
-    ssh \
-    sshd \
-    sshd_config \
-    ssh-keygen \
-    start-ssh
-endif
-
-# Config
-#PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
-
-#ifeq ($(ROM_BUILDTYPE),MICROG)
-#PRODUCT_PACKAGES += \
-#    MicrogUIOverlay
-#endif
-
-# Telephony extension
-#PRODUCT_PACKAGES += telephony-ext
-#PRODUCT_BOOT_JARS += telephony-ext
-
-# for fun
-#PRODUCT_PACKAGES += \
-    EggGame
 
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, vendor/omni/prebuilt/fonts/fonts.mk)
