3598f6db0b744391090a9ea87d524724ebd3bf67
micky387 <mickaelsaibi@free.fr>
2024-03-29 05:04:55 -0400

    onmi: Add qcom-caf wlan to soong namespaces
    Change-Id: I6b67b7932e27a7f1fbbb623e3e5b9e2103684c8c

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -94,3 +94,5 @@ $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/wlan
