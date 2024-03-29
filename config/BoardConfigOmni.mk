6d726a7e5054bfc1be8b61b088c51baa5930e8c5
micky387 <mickaelsaibi@free.fr>
2024-03-29 05:03:59 -0400

    omni: Move all Qcom board flags to BoardConfigQcom.mk
    It fix the board Qcom flag called to early and not added into soong, like PRODUCT_CFI_INCLUDE_PATHS and PRODUCT_SOONG_NAMESPACES
    
    Change-Id: Ia0d34e06319d12c0b4c4c863edf3b2d43b4e5956

--- config/BoardConfigOmni.mk
+++ config/BoardConfigOmni.mk
@@ -2,4 +2,8 @@ ifneq ($(TARGET_USES_KERNEL_PLATFORM),true)
 include vendor/omni/config/BoardConfigKernel.mk
 endif
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/omni/config/BoardConfigQcom.mk
+endif
+
 include vendor/omni/config/BoardConfigSoong.mk
