740cc21e4f51933fdb548db99235d5ae4372f899
Bruno Martins <bgcngm@gmail.com>
2024-06-23 11:54:20 +0100

    omni: Introduce RFS install_symlink targets
    The old way of creating RFS symlinks was preventing all Qualcomm targets
    to build hermetic partitions. All devices should now be able to drop
    BUILD_BROKEN_INCORRECT_PARTITION_IMAGES flag for good.
    
    Change-Id: I95076d884ab3c3d46b3875e77b9088f6df895c31

---
+++ qcom/common.mk
@@ -0,0 +1,107 @@
+# RFS APQ GNSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_apq_gnss_hlos_symlink \
+    rfs_apq_gnss_ramdumps_symlink \
+    rfs_apq_gnss_readonly_firmware_symlink \
+    rfs_apq_gnss_readonly_vendor_firmware_symlink \
+    rfs_apq_gnss_readwrite_symlink \
+    rfs_apq_gnss_shared_symlink
+
+# RFS MDM ADSP symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_adsp_hlos_symlink \
+    rfs_mdm_adsp_ramdumps_symlink \
+    rfs_mdm_adsp_readonly_firmware_symlink \
+    rfs_mdm_adsp_readonly_vendor_firmware_symlink \
+    rfs_mdm_adsp_readwrite_symlink \
+    rfs_mdm_adsp_shared_symlink
+
+# RFS MDM CDSP symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_cdsp_hlos_symlink \
+    rfs_mdm_cdsp_ramdumps_symlink \
+    rfs_mdm_cdsp_readonly_firmware_symlink \
+    rfs_mdm_cdsp_readonly_vendor_firmware_symlink \
+    rfs_mdm_cdsp_readwrite_symlink \
+    rfs_mdm_cdsp_shared_symlink
+
+# RFS MDM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_mpss_hlos_symlink \
+    rfs_mdm_mpss_ramdumps_symlink \
+    rfs_mdm_mpss_readonly_firmware_symlink \
+    rfs_mdm_mpss_readonly_vendor_firmware_symlink \
+    rfs_mdm_mpss_readwrite_symlink \
+    rfs_mdm_mpss_shared_symlink
+
+# RFS MDM SLPI symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_slpi_hlos_symlink \
+    rfs_mdm_slpi_ramdumps_symlink \
+    rfs_mdm_slpi_readonly_firmware_symlink \
+    rfs_mdm_slpi_readonly_vendor_firmware_symlink \
+    rfs_mdm_slpi_readwrite_symlink \
+    rfs_mdm_slpi_shared_symlink
+
+# RFS MDM TN symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_tn_hlos_symlink \
+    rfs_mdm_tn_ramdumps_symlink \
+    rfs_mdm_tn_readonly_firmware_symlink \
+    rfs_mdm_tn_readonly_vendor_firmware_symlink \
+    rfs_mdm_tn_readwrite_symlink \
+    rfs_mdm_tn_shared_symlink
+
+# RFS MDM WPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_wpss_hlos_symlink \
+    rfs_mdm_wpss_ramdumps_symlink \
+    rfs_mdm_wpss_readonly_firmware_symlink \
+    rfs_mdm_wpss_readonly_vendor_firmware_symlink \
+    rfs_mdm_wpss_readwrite_symlink \
+    rfs_mdm_wpss_shared_symlink
+
+# RFS MSM ADSP symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_adsp_hlos_symlink \
+    rfs_msm_adsp_ramdumps_symlink \
+    rfs_msm_adsp_readonly_firmware_symlink \
+    rfs_msm_adsp_readonly_vendor_firmware_symlink \
+    rfs_msm_adsp_readwrite_symlink \
+    rfs_msm_adsp_shared_symlink
+
+# RFS MSM CDSP symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_cdsp_hlos_symlink \
+    rfs_msm_cdsp_ramdumps_symlink \
+    rfs_msm_cdsp_readonly_firmware_symlink \
+    rfs_msm_cdsp_readonly_vendor_firmware_symlink \
+    rfs_msm_cdsp_readwrite_symlink \
+    rfs_msm_cdsp_shared_symlink
+
+# RFS MSM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_mpss_hlos_symlink \
+    rfs_msm_mpss_ramdumps_symlink \
+    rfs_msm_mpss_readonly_firmware_symlink \
+    rfs_msm_mpss_readonly_vendor_firmware_symlink \
+    rfs_msm_mpss_readwrite_symlink \
+    rfs_msm_mpss_shared_symlink
+
+# RFS MSM SLPI symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_slpi_hlos_symlink \
+    rfs_msm_slpi_ramdumps_symlink \
+    rfs_msm_slpi_readonly_firmware_symlink \
+    rfs_msm_slpi_readonly_vendor_firmware_symlink \
+    rfs_msm_slpi_readwrite_symlink \
+    rfs_msm_slpi_shared_symlink
+
+# RFS MSM WPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_wpss_hlos_symlink \
+    rfs_msm_wpss_ramdumps_symlink \
+    rfs_msm_wpss_readonly_firmware_symlink \
+    rfs_msm_wpss_readonly_vendor_firmware_symlink \
+    rfs_msm_wpss_readwrite_symlink \
+    rfs_msm_wpss_shared_symlink
