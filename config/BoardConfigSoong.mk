133cf23d0e00400f454fb5f37f4841751395c3f4
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 20:06:00 +0200

    omni: soong: Move QTI gralloc configs to select()
    Change-Id: Icb28d5c2aa5d51860799b87b7b9b04c48014f7f4
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,9 +32,6 @@ SOONG_CONFIG_omniGlobalVars += \
     target_init_vendor_lib \
     target_power_libperfmgr_mode_extension_lib \
     target_surfaceflinger_udfps_lib \
-    gralloc_handle_has_custom_content_md_reserved_size \
-    gralloc_handle_has_reserved_size \
-    gralloc_handle_has_ubwcp_format \
     healthd_use_battery_info \
     healthd_enable_op_fastchg \
     launcher3Gapps \
@@ -66,9 +63,6 @@ SOONG_CONFIG_omniQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_omniQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
 SOONG_CONFIG_omniGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_omniGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
-SOONG_CONFIG_omniGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
-SOONG_CONFIG_omniGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
-SOONG_CONFIG_omniGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_omniGlobalVars_healthd_use_battery_info := $(HEALTHD_USE_BATTERY_INFO)
 SOONG_CONFIG_omniGlobalVars_healthd_enable_op_fastchg := $(HEALTHD_ENABLE_OP_FASTCHG_CHECK)
 SOONG_CONFIG_omniGlobalVars_targetNeedsHWCOnFirstRef := $(TARGET_NEEDS_HWC_ONFIRSTREF)
@@ -84,9 +78,6 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB ?= libperfmgr-ext
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
-TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
-TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
-TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
 
 SOONG_CONFIG_omniGlobalVars_launcher3Gapps ?= false
 SOONG_CONFIG_omniGlobalVars_launcher3Mock ?= false
