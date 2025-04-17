a15cfb0978f9bde0053430e5d38141de16b15821
Aaron Kling <webgeek1234@gmail.com>
2025-04-16 20:59:01 -0500

    soong: Support prebuilt_kernel_headers
    This depends on a kernel headers tarball path being passed in
    TARGET_PREBUILT_KERNEL_HEADERS, in the style that the kleaf kernel
    platform build generates.
    
    Change-Id: Ib82659fa44ef221e73d0d129daafbaae6bad87a2
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -6,7 +6,8 @@ EXPORT_TO_SOONG := \
     KERNEL_MAKE_CMD \
     KERNEL_MAKE_FLAGS \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
+    TARGET_KERNEL_SOURCE \
+    TARGET_PREBUILT_KERNEL_HEADERS
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
