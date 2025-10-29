LineageOS/android_vendor_lineage

config: Use add_soong_config_namespace/add_soong_config_var macros
Test: m nothing and compare out/soong/soong.lineage_*.variables
Change-Id: I802f4f7eb37d520fd74599e2b673a5b2368c5d09

config/BoardConfigSoong.mk
@@ -15,16 +15,8 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += lineageVarsPlugin
-
-SOONG_CONFIG_lineageVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_lineageVarsPlugin += $(1)
-  SOONG_CONFIG_lineageVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+$(call add_soong_config_namespace,lineageVarsPlugin)
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lineageVarsPlugin,$(v))))
 
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)

omnirom/android_vendor_omni

Signed-off-by: micky387 <mickaelsaibi@free.fr>

config/BoardConfigSoong.mk
@@ -12,16 +12,8 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += omniromVarsPlugin
-
-SOONG_CONFIG_omniromVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_omniromVarsPlugin += $(1)
-  SOONG_CONFIG_omniromVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+$(call add_soong_config_namespace,omniromVarsPlugin)
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,omniromVarsPlugin,$(v))))
 
 SOONG_CONFIG_NAMESPACES += omniGlobalVars
 SOONG_CONFIG_omniGlobalVars += \