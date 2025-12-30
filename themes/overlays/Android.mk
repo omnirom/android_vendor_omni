7754583e8a81363304bc8e8751eedb8454bf022e
Michael Bestas <mkbestas@lineageos.org>
2025-12-30 02:20:39 +0200

    config: Stop building icon packs & shapes
    BP3A refactored ThemePicker so our additions are no longer visible in
    the UI. Additionally, the icon packs barely apply to icons since the
    compose rewrite in SystemUI. BP4A has added icon shape selection but
    implemented differently than what we had previously.
    
    Users that upgrade from 23.0 and had custom icons set won't be able to
    unset them, so stop building them.
    
    In the future, we could bring the missing icon shapes back but
    reimplemented to show in the new picker UI.
    
    Change-Id: If41c5990a67340c04ca251f267fdc8e2732bc864
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- themes/overlays/Android.mk
+++ themes/overlays/Android.mk
@@ -28,39 +28,6 @@ LOCAL_REQUIRED_MODULES += \
 	FontMuliSourceOverlay
 
 LOCAL_REQUIRED_MODULES += \
-	IconPackCircularAndroidOverlay \
-	IconPackCircularLauncherOverlay \
-	IconPackCircularSettingsOverlay \
-	IconPackCircularSystemUIOverlay \
-	IconPackFilledAndroidOverlay \
-	IconPackFilledLauncherOverlay \
-	IconPackFilledSettingsOverlay \
-	IconPackFilledSystemUIOverlay \
-	IconPackKaiAndroidOverlay \
-	IconPackKaiLauncherOverlay \
-	IconPackKaiSettingsOverlay \
-	IconPackKaiSystemUIOverlay \
-	IconPackRoundedAndroidOverlay \
-	IconPackRoundedLauncherOverlay \
-	IconPackRoundedSettingsOverlay \
-	IconPackRoundedSystemUIOverlay \
-	IconPackSamAndroidOverlay \
-	IconPackSamLauncherOverlay \
-	IconPackSamSettingsOverlay \
-	IconPackSamSystemUIOverlay \
-	IconPackVictorAndroidOverlay \
-	IconPackVictorLauncherOverlay \
-	IconPackVictorSettingsOverlay \
-	IconPackVictorSystemUIOverlay
-
-LOCAL_REQUIRED_MODULES += \
-	IconShapeCircleOverlay \
-	IconShapeSquareOverlay \
-	IconShapeSquircleOverlay \
-	IconShapeTeardropOverlay \
-	IconShapePebbleOverlay
-
-LOCAL_REQUIRED_MODULES += \
 	PrimaryColorDarkBlueOverlay \
 	PrimaryColorDarkGrayOverlay
 
